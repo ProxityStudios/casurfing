8052a73ff3a1719d5e043e684e690d7185ccda9e
Ticho <103999185+Tichooo@users.noreply.github.com>
2023-01-02 14:37:38 +0000

    Lint the codes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const Index = () => {
 
   return (
      <div className="flex items-center justify-center h-full w-full">
-       <Link href={routes.getConversation()} className="px-6 py-3 rounded-full bg-teal-200 text-black text-lg font-medium hover:bg-teal-300 active:bg-teal-200 duration-200">
+       <Link href={routes.getHome()} className="px-6 py-3 rounded-full bg-teal-200 text-black text-lg font-medium hover:bg-teal-300 active:bg-teal-200 duration-200">
         Use The Application
       </Link>
      </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,16 @@
-import routes from "@/utils/routes";
-import Link from "next/link";
+import Link from 'next/link';
+import routes from '@/utils/routes';
 
 function Header() {
-  return <header className="container mx-auto">
-    <nav className="h-16 flex items-center justify-between">
-      <Link href={routes.getHome()}>{"<"} Back</Link>
-      <div>
-        <Link href={routes.getProfile()}>Profile</Link>
-      </div>
-    </nav>
-  </header>
-};
+  return (
+    <header className="container mx-auto">
+      <nav className="h-16 flex items-center justify-between">
+        <Link href={routes.getHome()}>{'<'} Back</Link>
+        <div>
+          <Link href={routes.getProfile()}>Profile</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
 export default Header;
